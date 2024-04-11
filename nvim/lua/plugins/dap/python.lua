77af7bf8124b02387affae90c2338945b033a497
xuekai <3440395@qq.com>
2024-04-11 16:22:13 +0800

    python debug with args

--- nvim/lua/plugins/dap/python.lua
+++ nvim/lua/plugins/dap/python.lua
@@ -4,12 +4,31 @@ dap.adapters.python = {
 	command = "python3",
 	args = { '-m', 'debugpy.adapter' },
 }
+local function get_arguments()
+	return coroutine.create(function(dap_run_co)
+		local args = {}
+		vim.ui.input({ prompt = "Args: " }, function(input)
+			args = vim.split(input or "", " ")
+			coroutine.resume(dap_run_co, args)
+		end)
+	end)
+end
 dap.configurations.python = {
 	{
 		type = "python",
 		request = "launch",
-		name = "launch file",
+		name = "Debug",
 		program = "${file}",
+		pythonPath = function()
+			return "python3"
+		end
+	},
+	{
+		type = "python",
+		request = "launch",
+		name = "Debug (Arguments)",
+		program = "${file}",
+		args = get_arguments,
 		pythonPath = function()
 			return "python3"
 		end
