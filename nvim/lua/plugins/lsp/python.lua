13468484527d9d97bb87f72f40aaf5d5d6e4196a
xuekai <3440395@qq.com>
2024-04-12 09:06:14 +0800

    F5运行

--- nvim/lua/plugins/lsp/python.lua
+++ nvim/lua/plugins/lsp/python.lua
@@ -1,2 +1,2 @@
 require('lspconfig').pylsp.setup {}
-vim.api.nvim_exec([[ autocmd FileType python nnoremap <buffer> <F5> :!python3 %<CR> ]], false)
+vim.api.nvim_exec([[ autocmd FileType python nnoremap <buffer> <F5> :!python3 %<CR>]], false)
