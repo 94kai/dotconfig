dabd76829172d978c4d1ef4dd62eb497126678c9
xuekai <3440395@qq.com>
2024-04-13 18:24:09 +0800

    theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -7,9 +7,9 @@ vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
 --     transparent = false, -- Enable this to disable setting the background color
 --   })
 --       vim.cmd("colorscheme tokyonight")
---       vim.cmd("highlight Visual guibg=#3498db")-- 选中的背景色
---       vim.cmd("highlight Visual guifg=white")-- 选中的前景色
---       vim.cmd("highlight Comment guifg=#57606f")-- 注释的前景色
---       vim.cmd("highlight CursorLine guibg=#41435e")-- 当前行高亮的颜色
---       vim.cmd("highlight CursorColumn guibg=#41435e")-- 当前列高亮的颜色
+vim.cmd("highlight Visual guibg=#3498db")             -- 选中的背景色
+vim.cmd("highlight Visual guifg=white")               -- 选中的前景色
+vim.cmd("highlight Comment guifg=#57606f")            -- 注释的前景色
+vim.cmd("highlight CursorLine guibg=#41435e")         -- 当前行高亮的颜色
+vim.cmd("highlight CursorColumn guibg=#41435e")       -- 当前列高亮的颜色
 -- end
