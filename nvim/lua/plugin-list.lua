64d57e861881565168389c7ffae3e87284e37957
xuekai <3440395@qq.com>
2024-05-22 09:44:32 +0800

    flash+1

--- nvim/lua/plugin-list.lua
+++ nvim/lua/plugin-list.lua
@@ -265,6 +265,6 @@ return {
 		-- 	{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
 		-- 	{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 		-- 	{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-		-- },
+		},
 	}
 }
