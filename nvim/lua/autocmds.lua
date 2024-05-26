d30ae164b9e79bf003d744c549cb77e4dc470556
xuekai <3440395@qq.com>
2024-05-26 18:43:23 +0800

    Rime代替autocmd

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,16 +3,6 @@ local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local lastTime = os.time()
-autocmd("InsertLeave", {
-	group = myAutoGroup,
-	callback = function()
-		-- 用job去优化批量任务导致的耗时问题
-		if os.time() - lastTime > 1 then
-			vim.cmd("call jobstart('im-select com.apple.keylayout.ABC')")
-			lastTime = os.time()
-		end
-	end
-})
 
 
 -- https://www.reddit.com/r/neovim/comments/zc720y/tip_to_manage_hlsearch/
