e97636c16cba4c383832244d38335fbaaf9046a0
xuekai <3440395@qq.com>
2024-06-05 11:01:44 +0800

    python3->python

--- nvim/lua/plugins/dap/python.lua
+++ nvim/lua/plugins/dap/python.lua
@@ -1,36 +1,36 @@
-local dap = require('dap')
+local dap = require("dap")
 dap.adapters.python = {
-	type = 'executable',
-	command = "python3",
-	args = { '-m', 'debugpy.adapter' },
+  type = "executable",
+  command = "python",
+  args = { "-m", "debugpy.adapter" },
 }
 local function get_arguments()
-	return coroutine.create(function(dap_run_co)
-		local args = {}
-		vim.ui.input({ prompt = "Args: " }, function(input)
-			args = vim.split(input or "", " ")
-			coroutine.resume(dap_run_co, args)
-		end)
-	end)
+  return coroutine.create(function(dap_run_co)
+    local args = {}
+    vim.ui.input({ prompt = "Args: " }, function(input)
+      args = vim.split(input or "", " ")
+      coroutine.resume(dap_run_co, args)
+    end)
+  end)
 end
 dap.configurations.python = {
-	{
-		type = "python",
-		request = "launch",
-		name = "Debug",
-		program = "${file}",
-		pythonPath = function()
-			return "python3"
-		end
-	},
-	{
-		type = "python",
-		request = "launch",
-		name = "Debug (Arguments)",
-		program = "${file}",
-		args = get_arguments,
-		pythonPath = function()
-			return "python3"
-		end
-	},
+  {
+    type = "python",
+    request = "launch",
+    name = "Debug",
+    program = "${file}",
+    pythonPath = function()
+      return "python"
+    end,
+  },
+  {
+    type = "python",
+    request = "launch",
+    name = "Debug (Arguments)",
+    program = "${file}",
+    args = get_arguments,
+    pythonPath = function()
+      return "python"
+    end,
+  },
 }
--- nvim/lua/plugins/lsp/python.lua
+++ nvim/lua/plugins/lsp/python.lua
@@ -1,2 +1,2 @@
-require('lspconfig').pylsp.setup {}
-vim.api.nvim_exec([[ autocmd FileType python nnoremap <buffer> <F5> :!python3 %<CR>]], false)
+require("lspconfig").pylsp.setup({})
+vim.api.nvim_exec([[ autocmd FileType python nnoremap <buffer> <F5> :!python %<CR>]], false)
