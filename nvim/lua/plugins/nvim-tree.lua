cb174ad5e9cfe7c3ccc8575fd38ab5d9acf722fa
xuekai <3440395@qq.com>
2024-06-25 16:01:00 +0800

    nvim-tree;输入法vim-mode

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,52 +1,50 @@
 -- 目录
 local nvimTree = pRequire("nvim-tree")
 if nvimTree then
-    keymap("n", "<C-f>", "<CMD>NvimTreeToggle<CR>")
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+  keymap("n", "<C-f>", "<CMD>NvimTreeToggle<CR>")
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-    nvimTree.setup({
-        update_focused_file = {
-            enable = true,
-            update_root = true,
+  nvimTree.setup({
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+    },
+    git = {
+      enable = true,
+      ignore = true,
+    },
+    filters = {
+      git_ignored = false,
+      dotfiles = false,
+    },
+    view = {
+      width = 34,
+      -- or 'right'
+      side = "left",
+      number = false,
+      relativenumber = false,
+      signcolumn = "yes",
+    },
+    actions = {
+      open_file = {
+        resize_window = true,
+        quit_on_open = false,
+      },
+    },
+    renderer = {
+      root_folder_label = ":~:s?$?/..?",
+      indent_markers = {
+        enable = true,
+        inline_arrows = true,
+        icons = {
+          corner = "└",
+          edge = "│",
+          item = "│",
+          bottom = "─",
+          none = " ",
         },
-        git = {
-            enable = true,
-            ignore = true,
-        },
-        filters = {
-			git_ignored = false,
-			dotfiles = false,
-        },
-        view = {
-            width = 34,
-            -- or 'right'
-            side = "left",
-            number = false,
-            relativenumber = false,
-            signcolumn = "yes",
-        },
-        actions = {
-            open_file = {
-                resize_window = true,
-                quit_on_open = false,
-            },
-        },
-        renderer = {
-            root_folder_label = true,
-            indent_markers = {
-                enable = false,
-                icons = {
-                    corner = "└ ",
-                    edge = "│ ",
-                    none = "  ",
-                },
-            },
-            icons = {
-                webdev_colors = true,
-                git_placement = "after",
-            },
-        },
-    })
-
+      },
+    },
+  })
 end
