f002050088abf28eb62cf51f9cc16105fef04551
xuekai <3440395@qq.com>
2024-07-11 17:04:39 +0800

    close rainbowcolor of indent

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -10,26 +10,26 @@ local highlight = {
   "RainbowCyan",
 }
 
-local hooks = require("ibl.hooks")
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-
-hooks.register(hooks.type.ACTIVE, function(num)
-  local fsize = vim.fn.getfsize(vim.fn.expand("%:p"))
-  if fsize > 1024 * 100 then
-    return false
-  end
-  return true
-end)
+-- local hooks = require("ibl.hooks")
+-- -- create the highlight groups in the highlight setup hook, so they are reset
+-- -- every time the colorscheme changes
+-- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+--   vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+--   vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+--   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+--   vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+--   vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+--   vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+--   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+-- end)
+--
+-- hooks.register(hooks.type.ACTIVE, function(num)
+--   local fsize = vim.fn.getfsize(vim.fn.expand("%:p"))
+--   if fsize > 1024 * 100 then
+--     return false
+--   end
+--   return true
+-- end)
 require("ibl").setup({
   scope = {
     show_start = true,
@@ -56,12 +56,12 @@ require("ibl").setup({
       "''",
     },
   },
-  -- indent = {
-  --   -- char = '¦'
-  --   -- char = '┆'
-  --   -- char = '│'
-  --   -- char = "⎸",
-  --   char = "▏",
-  --   -- char = "*",
-  -- },
+  indent = {
+    -- char = '¦'
+    -- char = '┆'
+    -- char = '│'
+    -- char = "⎸",
+    char = "▏",
+    -- char = "*",
+  },
 })
