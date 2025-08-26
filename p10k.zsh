ba9c8fe978bf9016949b59f3329c5295fa1e176a
xuekai.xk <xuekai.xk@bytedance.com>
2025-08-26 10:16:45 +0800

    p10k

--- p10k.zsh
+++ p10k.zsh
@@ -49,6 +49,7 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
+    time                    # current time
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
@@ -97,7 +98,6 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
     # vpn_ip                # virtual private network indicator
-    time                    # current time
     load                  # CPU load
     disk_usage            # disk usage
     ram                   # free RAM
--- zshrc
+++ zshrc
@@ -165,7 +165,6 @@ fi
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-. "$HOME/.local/bin/env"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
